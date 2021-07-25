b993d9d446514d1969b4a30db6f98c7a868c362a
Vladislav Lisyanskiy <vladislav.lisyanskiy@gmail.com>
2021-07-25 15:42:12 +0300

    Fixed generated files

--- Scripts/autogenerated_files.sh
+++ Scripts/autogenerated_files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GENERATED_DIR="./Wake\ On\ Lan/Modules/WOLResources/Generated"
+GENERATED_DIR="./Wake\ On\ Lan/Modules/WOLResources/Sources/Generated"
 eval mkdir -p $GENERATED_DIR
 eval touch $GENERATED_DIR/Strings+Generated.swift
 eval touch $GENERATED_DIR/XCAssets+Generated.swift
