43a04b9d910c96c7c41c5737d6e5f69c232a735b
Vladislav Lisyanskiy <vladislav.lisyanskiy@gmail.com>
2021-08-01 15:39:39 +0300

    Maybe it is required for realtime sync

--- Wake On Lan/Modules/CoreDataService/Sources/CoreDataServiceProtocol.swift
+++ Wake On Lan/Modules/CoreDataService/Sources/CoreDataServiceProtocol.swift
@@ -27,7 +27,9 @@ public protocol CoreDataServiceProtocol {
 extension CoreDataServiceProtocol {
 
     public var mainContext: NSManagedObjectContext {
-        persistentContainer.viewContext
+        let context = persistentContainer.viewContext
+        context.automaticallyMergesChangesFromParent = true
+        return context
     }
 
     public func createHostContainer(completion: @escaping () -> Void) {
@@ -54,6 +56,7 @@ extension CoreDataServiceProtocol {
     public func createChildConcurrentContext() -> NSManagedObjectContext {
         let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
         context.parent = mainContext
+        context.automaticallyMergesChangesFromParent = true
         DDLogVerbose("Child concurrent context was created")
         return context
     }
