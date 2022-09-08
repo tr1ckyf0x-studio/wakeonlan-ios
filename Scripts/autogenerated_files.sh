ada6247f265d660d113999d635048eda855a19ac
Vladislav Lisyanskiy <vladislav.lisyanskiy@gmail.com>
2022-09-08 08:19:38 +0400

    Fixed Sourcery generation

--- Scripts/autogenerated_files.sh
+++ Scripts/autogenerated_files.sh
@@ -6,4 +6,14 @@ eval touch $GENERATED_DIR/Strings+Generated.swift
 eval touch $GENERATED_DIR/XCAssets+Generated.swift
 
 swiftgen config run --config Wake\ on\ LAN/Modules/**/swiftgen.yml
-sourcery -v --config Wake\ on\ LAN/Modules/**/sourcery.yml
+
+SOURCERY_MODULES=(
+	"AddHost"
+	)
+
+MODULES_FOLDER="./Wake On Lan/Modules"
+
+for MODULE in "${SOURCERY_MODULES[@]}"; do
+	MODULE_DIR="${MODULES_FOLDER}/${MODULE}"
+	sourcery -v --config "${MODULE_DIR}/sourcery.yml"
+done
