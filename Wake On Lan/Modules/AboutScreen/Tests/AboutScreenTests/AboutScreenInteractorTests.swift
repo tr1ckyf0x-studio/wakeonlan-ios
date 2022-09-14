7d994c7b3584ce49d99ae34e7d2987625adb6960
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2022-09-15 06:30:19 +1200

    Added test for  AboutScreenInteractor

--- Wake On Lan/Modules/AboutScreen/Tests/AboutScreenTests/AboutScreenInteractorTests.swift
+++ Wake On Lan/Modules/AboutScreen/Tests/AboutScreenTests/AboutScreenInteractorTests.swift
@@ -1,16 +1,36 @@
 import XCTest
 
 @testable import AboutScreen
+@testable import WOLResources
 import WOLResourcesMock
 
 final class AboutScreenInteractorTests: XCTestCase {
 
     var sut: AboutScreenInteractor!
     var bundleInfoProviderMock: ProvidesBundleInfoMock!
+    var presenterMock: AboutScreenInteractorOutputMock!
 
     override func setUp() {
         super.setUp()
         bundleInfoProviderMock = ProvidesBundleInfoMock()
+        presenterMock = AboutScreenInteractorOutputMock()
         sut = AboutScreenInteractor(bundleInfoProvider: bundleInfoProviderMock)
+        sut.presenter = presenterMock
+    }
+
+    func testFetchBundleInfo() {
+        let bundleInfo = BundleInfo(
+            displayName: "",
+            identifier: "",
+            name: "",
+            version: "",
+            build: "",
+            appFonts: nil
+        )
+
+        bundleInfoProviderMock.fetchBundleInfoReturnValue = bundleInfo
+        sut.fetchBundleInfo()
+        XCTAssertEqual(presenterMock.interactorDidFetchBundleInfoCallsCount, 1, "presenter must be called once")
+        XCTAssertEqual(presenterMock.interactorDidFetchBundleInfoReceivedArguments?.bundleInfo.name, bundleInfo.name, "interactor must call presenter with bundle info received from bundleInfoProvider")
     }
 }
