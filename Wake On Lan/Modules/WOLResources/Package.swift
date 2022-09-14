89634fe7b549f3ac2ef59b7854b73075a2c123e7
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2022-09-15 05:39:01 +1200

    Added WOLResourcesMock

--- Wake On Lan/Modules/WOLResources/Package.swift
+++ Wake On Lan/Modules/WOLResources/Package.swift
@@ -11,11 +11,19 @@ let package = Package(
         .library(
             name: "WOLResources",
             targets: ["WOLResources"]
+        ),
+        .library(
+            name: "WOLResourcesMock",
+            targets: ["WOLResourcesMock"]
         )
     ],
     targets: [
         .target(
             name: "WOLResources"
+        ),
+        .target(
+            name: "WOLResourcesMock",
+            dependencies: ["WOLResources"]
         )
     ]
 )
