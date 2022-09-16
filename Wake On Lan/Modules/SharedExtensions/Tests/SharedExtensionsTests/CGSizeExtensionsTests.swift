51d255bfee066866ec46b67672bebe927bef894e
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2022-09-17 06:57:30 +1200

    CGSize Extension Test

---
+++ Wake On Lan/Modules/SharedExtensions/Tests/SharedExtensionsTests/CGSizeExtensionsTests.swift
@@ -0,0 +1,32 @@
+import XCTest
+import CoreGraphics
+
+@testable import SharedExtensions
+
+final class CGSizeExtensionsTests: XCTestCase {
+    func testInverse() {
+        XCTAssertEqual(TestData.sizePositiveWidth.inversed, TestData.expectedSizePositiveWidth, "it must inverse sign")
+        XCTAssertEqual(TestData.sizeNegativeWidth.inversed, TestData.expectedSizeNegativeWidth, "it must inverse sign")
+    }
+}
+
+extension CGSizeExtensionsTests {
+    enum TestData {
+        static let sizePositiveWidth = CGSize(
+            width: 5,
+            height: -5
+        )
+        static let expectedSizePositiveWidth = CGSize(
+            width: -5,
+            height: 5
+        )
+        static let sizeNegativeWidth = CGSize(
+            width: -5,
+            height: 5
+        )
+        static let expectedSizeNegativeWidth = CGSize(
+            width: 5,
+            height: -5
+        )
+    }
+}
