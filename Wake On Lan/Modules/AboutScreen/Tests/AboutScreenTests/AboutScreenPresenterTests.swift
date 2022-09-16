78c43dad00450fa8cf7ecdf4a69d8f91b179ed69
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2022-09-16 13:55:09 +1200

    Tested  Presenter viewDidLoad

--- Wake On Lan/Modules/AboutScreen/Tests/AboutScreenTests/AboutScreenPresenterTests.swift
+++ Wake On Lan/Modules/AboutScreen/Tests/AboutScreenTests/AboutScreenPresenterTests.swift
@@ -1,20 +1,30 @@
 import XCTest
 
 @testable import AboutScreen
+@testable import WOLResources
 import SharedRouterMock
 
 final class AboutScreenPresenterTests: XCTest {
     var sut: AboutScreenPresenter!
     var interactorMock: AboutScreenInteractorInputMock!
-    var routerMock: RouterMock!
+    var viewController: AboutScreenViewInputMock!
+    // var routerMock: RouterMock!
 
     override func setUp() {
         super.setUp()
         interactorMock = AboutScreenInteractorInputMock()
-        routerMock = RouterMock()
+        viewController = AboutScreenViewInputMock()
+        // routerMock = RouterMock()
         sut = AboutScreenPresenter()
         sut.interactor = interactorMock
         sut.interactor = interactorMock
-        sut.router = routerMock
+        // sut.router = routerMock
+    }
+
+    func testViewDidLoad() {
+        // when
+        sut.viewDidLoad(viewController)
+        // then
+        XCTAssertEqual(interactorMock.fetchBundleInfoCallsCount, 1, "Interactor must be called once")
     }
 }
