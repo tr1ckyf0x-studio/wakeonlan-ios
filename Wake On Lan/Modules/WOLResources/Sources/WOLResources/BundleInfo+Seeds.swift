c5248fbf099090b94a6691aa88fdb956ee2bbef0
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2022-09-27 13:13:23 +1300

    Created testData for BundleInfo

---
+++ Wake On Lan/Modules/WOLResources/Sources/WOLResources/BundleInfo+Seeds.swift
@@ -0,0 +1,12 @@
+import Foundation
+import SharedExtensions
+
+extension BundleInfo {
+    static let testData = BundleInfo(
+        displayName: String().randomString(length: 10),
+        identifier: String().randomString(length: 10),
+        name: String().randomString(length: 10),
+        version: String().randomString(length: 10),
+        build: String().randomString(length: 10),
+        appFonts: nil)
+}
