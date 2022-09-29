05fe170a60f70217434a1e0aa78cd650765e43d0
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2022-09-30 06:25:19 +1300

    Removed the random string extension, used UUID instead

---
+++ Wake On Lan/Modules/SharedExtensions/Sources/SharedExtensions/String+Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  String+Extensions.swift
+//  SharedExtensions
+//
+//  Created by Dmitry on 19.12.2020.
+//  Copyright © 2020 Владислав Лисянский. All rights reserved.
+//
+
+public extension String {
+
+    static let empty: String = .init()
+
+    func matches(_ regex: String) -> Bool {
+        range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil
+    }
+
+    func formatted(by mask: String, _ separator: String) -> String {
+        let cleanString = components(separatedBy: separator).joined()
+        var result = String.empty
+        var index = cleanString.startIndex
+        for char in mask where index < cleanString.endIndex {
+            if char == Configuration.maskSymbol {
+                result.append(cleanString[index])
+                index = cleanString.index(after: index)
+            } else {
+                result.append(char)
+            }
+        }
+
+        return result
+    }
+
+    private enum Configuration {
+        static let maskSymbol: Character = "X"
+    }
+
+}
